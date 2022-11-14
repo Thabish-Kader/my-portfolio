02002894ee16ec02c1468bc93af736d2e985da79
Thabish <kadertabish2@gmail.com>
2022-11-14 11:56:41 +0400

    circle-animation
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- components/CircleAinmation.tsx
+++ components/CircleAinmation.tsx
@@ -1,7 +1,24 @@
 import React from "react";
-
+import { motion } from "framer-motion";
 type Props = {};
 
 export const CircleAinmation = (props: Props) => {
-	return <div>CircleAinmation</div>;
+	return (
+		<motion.div className="absolute top-0 h-screen flex items-center justify-center">
+			<div className=" relative flex items-center justify-center ">
+				<div className="absolute h-[250px] animate-ping border-[#333333] w-[250px] rounded-full border" />
+				<div className="absolute h-[300px] animate-ping w-[300px] border border-[#333333] rounded-full" />
+				<div className="absolute h-[500px] animate-pulse w-[500px] border border-[#333333] rounded-full" />
+				<div className="absolute h-[600px] animate-pulse w-[600px] border border-purple-500 rounded-full" />
+				<div className="absolute h-[800px] animate-pulse w-[800px] border border-[#333333] rounded-full" />
+			</div>
+		</motion.div>
+		// <motion.div className="relative flex items-center justify-center">
+		// 	<div className="absolute mt-52 h-[200px] w-[200px] animate-ping rounded-full border border-[#333333]" />
+		// 	<div className="absolute mt-52 h-[300px] w-[300px] animate-ping rounded-full border border-[#333333]" />
+		// 	<div className="absolute mt-52 h-[500px] w-[500px] animate-ping rounded-full border border-[#333333]" />
+		// 	<div className="absolute mt-52 h-[650px] w-[650px] animate-pulse rounded-full border border-[#F7AB0A] opacity-20" />
+		// 	<div className="absolute mt-52 h-[800px] w-[800px] animate-pulse rounded-full border border-[#333333] " />
+		// </motion.div>
+	);
 };
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,27 +1,26 @@
 import React from "react";
-import { Typewriter } from "react-simple-typewriter";
+import { Typewriter, useTypewriter } from "react-simple-typewriter";
+import { CircleAinmation } from "./CircleAinmation";
 type Props = {};
 
 export const Hero = (props: Props) => {
+	const [text, count] = useTypewriter({
+		words: [
+			"Hi, names <Thabish/>",
+			"Loves Tailwind",
+			"Grateful to TypeScript",
+			"Enjoys Next JS",
+		],
+		loop: true,
+		delaySpeed: 2000,
+	});
 	return (
-		<section id="hero" className="inset-0 absolute">
-			<div className="flex items-center h-screen justify-center">
-				<div>
-					<p className="text-lg">
-						<Typewriter
-							words={[
-								"Hi, names <Thabish/>",
-								"Loves Tailwind",
-								"Grateful to TypeScript",
-								"Enjoys Next JS",
-							]}
-							loop={true}
-							cursor
-							cursorStyle="_"
-							cursorColor="purple"
-						/>
-					</p>
-				</div>
+		<section id="hero" className="snap-center">
+			<div className="flex flex-col overflow-hidden text-center items-center h-screen justify-center">
+				<CircleAinmation />
+				<h1>
+					<span>{text}</span>
+				</h1>
 			</div>
 		</section>
 	);
