dbd1b2dabb92b0bcf839bfc7dc22f6a79540fa2b
Thabish <kadertabish2@gmail.com>
2022-11-15 11:54:07 +0400

    animation-fix
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- components/CircleAinmation.tsx
+++ components/CircleAinmation.tsx
@@ -8,17 +8,14 @@ export const CircleAinmation = (props: Props) => {
 			initial={{ opacity: 0 }}
 			animate={{ scale: [2, 2, 1], opacity: 1 }}
 			transition={{ duration: 2 }}
-			className="absolute top-0 h-screen flex items-center justify-center"
+			className="relative top-[15%] flex items-center justify-center"
 		>
-			<div className=" relative flex items-center justify-center ">
-				<div className="absolute h-[250px] animate-ping border-[#333333] w-[250px] rounded-full border" />
-				<div className="absolute h-[300px] animate-ping w-[300px] border border-[#333333] rounded-full" />
-				<div className="absolute h-[500px] animate-pulse w-[500px] border border-[#333333] rounded-full" />
-				<div className="absolute h-[600px] animate-pulse w-[600px] border border-purple-500 rounded-full" />
-				<div className="absolute h-[800px] animate-pulse w-[800px] border border-[#333333] rounded-full" />
-			</div>
+			<div className="absolute h-[250px] animate-ping border-purple-500/50 w-[250px] rounded-full border" />
+			<div className="absolute h-[300px] animate-ping w-[300px] border border-purple-500/50 rounded-full" />
+			<div className="absolute h-[500px] animate-pulse w-[500px] border border-purple-500/10 rounded-full" />
+			<div className="absolute h-[600px] animate-pulse w-[600px] border border-purple-500 rounded-full" />
+			<div className="absolute h-[800px] animate-pulse w-[800px] border border-purple-500/20 rounded-full" />
 		</motion.div>
-		// <motion.div className="relative flex items-center justify-center">
 		// 	<div className="absolute mt-52 h-[200px] w-[200px] animate-ping rounded-full border border-[#333333]" />
 		// 	<div className="absolute mt-52 h-[300px] w-[300px] animate-ping rounded-full border border-[#333333]" />
 		// 	<div className="absolute mt-52 h-[500px] w-[500px] animate-ping rounded-full border border-[#333333]" />
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,8 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
-import { Typewriter, useTypewriter } from "react-simple-typewriter";
+import { Cursor, Typewriter, useTypewriter } from "react-simple-typewriter";
+import avatar from "../public/assets/avatar.png";
 import { CircleAinmation } from "./CircleAinmation";
 type Props = {};
 
@@ -16,11 +19,41 @@ export const Hero = (props: Props) => {
 	});
 	return (
 		<section id="hero" className="snap-center">
-			<div className="flex flex-col overflow-hidden text-center items-center h-screen justify-center">
+			<div className="flex h-screen flex-col items-center justify-center space-y-8 overflow-hidden text-center">
 				<CircleAinmation />
-				<h1>
-					<span>{text}</span>
-				</h1>
+				<div className="relative h-40 w-40 my-2">
+					<Image
+						src={avatar}
+						alt="/avatar"
+						fill
+						className="object-cover rounded-full"
+					/>
+				</div>
+				<div className="z-10">
+					<h2 className="text-gray-600 text-lg tracking-[15px] uppercase font-bold">
+						Web Developer
+					</h2>
+
+					<h1 className="pt-1 text-2xl text-purple-500 font-semibold lg:text-4xl">
+						<span>{text}</span>
+						{/* Make sure to add this line or else the button will jump up and down */}
+						<Cursor cursorColor="#8547bf" />
+					</h1>
+					<div className="space-x-3 my-2">
+						<Link href="#about">
+							<button className="hero-btn">About</button>
+						</Link>
+						<Link href="#skills">
+							<button className="hero-btn">Skills</button>
+						</Link>
+						<Link href="#projects">
+							<button className="hero-btn">Projects</button>
+						</Link>
+						<Link href="#contact">
+							<button className="hero-btn">Contact Me</button>
+						</Link>
+					</div>
+				</div>
 			</div>
 		</section>
 	);
