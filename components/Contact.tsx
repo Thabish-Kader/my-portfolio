8aba54934876d710c9053151c6b53242bd34475c
Thabish <kadertabish2@gmail.com>
2022-11-17 14:37:13 +0400

    form-added
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,10 +1,24 @@
 import React from "react";
 import { motion } from "framer-motion";
+import { useForm, SubmitHandler } from "react-hook-form";
+
 type Props = {};
+type Inputs = {
+	name: string;
+	email: string;
+	subject: string;
+	message: string;
+};
 
 export const Contact = (props: Props) => {
+	const { register, handleSubmit, reset } = useForm<Inputs>();
+	const onSubmit: SubmitHandler<Inputs> = (formData) => {
+		window.location.href = `mailto:kadertabish@gmail.com?subject=${formData.subject}&body=Hi, my name is${formData.name} ${formData.message} (${formData.email})`;
+		reset();
+	};
 	return (
 		<motion.section
+			id="contact"
 			initial={{ opacity: 0 }}
 			whileInView={{ opacity: 1 }}
 			transition={{ duration: 3 }}
@@ -19,31 +33,41 @@ export const Contact = (props: Props) => {
 					<span className="text-purple-500 animate-pulse">
 						amazing
 					</span>
-					<form className="flex mt-10 flex-col justify-center space-y-5 w-fit">
+					<form
+						onClick={handleSubmit(onSubmit)}
+						className="flex mt-10 flex-col justify-center space-y-5 w-fit"
+					>
 						<div className="flex justify-center space-x-2">
 							<input
+								{...register("name")}
 								placeholder="Name"
 								className="contact-inputs"
 								type="text"
 							/>
 							<input
+								{...register("email")}
 								placeholder="Email"
 								className="contact-inputs"
-								type="text"
+								type="email"
 							/>
 						</div>
 						<input
+							{...register("subject")}
 							placeholder="Subject"
 							className="contact-inputs"
 							type="text"
 						/>
 						<textarea
+							{...register("message")}
 							placeholder="Message"
 							className="contact-inputs"
 							name=""
 							id=""
 						/>
-						<button className="bg-purple-500 animate-pulse p-2 rounded-lg text-black hover:bg-white duration-500 ">
+						<button
+							type="submit"
+							className="bg-purple-500 font-semibold uppercase animate-pulse p-2 rounded-lg text-black hover:bg-white duration-500 "
+						>
 							Submit
 						</button>
 					</form>
