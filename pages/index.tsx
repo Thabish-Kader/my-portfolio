e734e60d0ac0dd1c1602dd176eeec6fe929f2026
Thabish <thabish@Chips-and-Dips.local>
2024-10-22 15:57:45 +0530

    oia script added
    Signed-off-by: Thabish <thabish@Chips-and-Dips.local>

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,28 +7,32 @@ import { Projects } from "../components/Projects";
 import { Skills } from "../components/Skills";
 
 export default function Home() {
-	return (
-		<div className="bg-[#0a192f] h-screen text-white scroll-smooth snap-y snap-mandatory overflow-scroll">
-			<Head>
-				<title>Thabish&apos;s Protfolio</title>
-				<meta />
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
+  return (
+    <div className="bg-[#0a192f] h-screen text-white scroll-smooth snap-y snap-mandatory overflow-scroll">
+      <Head>
+        <title>Thabish&apos;s Protfolio</title>
+        <meta />
+        <link rel="icon" href="/favicon.ico" />
+        <script
+          async
+          src="https://scripts.inopenapp.com/dead90c9-5715-47ce-8c23-a523e1989f08.js"
+        ></script>
+      </Head>
 
-			{/* Header */}
-			<Header />
-			{/* Hero */}
-			<Hero />
-			{/* About */}
-			<About />
-			{/* Skills */}
-			<Skills />
-			{/* Projects */}
-			<Projects />
-			{/* Contact Me */}
-			<Contact />
+      {/* Header */}
+      <Header />
+      {/* Hero */}
+      <Hero />
+      {/* About */}
+      <About />
+      {/* Skills */}
+      <Skills />
+      {/* Projects */}
+      <Projects />
+      {/* Contact Me */}
+      <Contact />
 
-			<div className=" animate-pulse absolute top-0 bg-[#64ffda] right-10 h-screen xl:w-[1px] "></div>
-		</div>
-	);
+      <div className=" animate-pulse absolute top-0 bg-[#64ffda] right-10 h-screen xl:w-[1px] "></div>
+    </div>
+  );
 }
